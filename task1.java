4108a7ae9536dbbd577fdc0a1e735c999ec41764
Dima <d.nikulin275@gmail.com>
2023-03-11 23:38:13 +0300

    Homework 11.03.2023

--- task1.java
+++ task1.java
@@ -1,34 +1,21 @@
-// Реализуйте алгоритм сортировки пузырьком числового массива (введён вами),
-// результат после каждой итерации (после перемещения числа) запишите в лог-файл.
+//Пусть дан произвольный список целых чисел, удалить из него чётные числа
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.logging.*;
-
+import java.util.ArrayList;
+import java.util.Random;
 
 public class task1 {
-    public static void main(String[] args) throws IOException {
-        int[] mas = {5, 2, 1, 3, 9, 0, 4, 6, 8, 7};
-        Logger logger = Logger.getLogger(task1.class.getName());
-        FileHandler fh = new FileHandler("log.txt");
-        logger.addHandler(fh);
-        SimpleFormatter sFormat = new SimpleFormatter(); 
-        fh.setFormatter(sFormat);
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
-                    isSorted = false;
- 
-                    buf = mas[i];
-                    logger.log(Level.INFO, "Число было перемещенно");
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
-                }
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            myList.add(new Random().nextInt(0, 100));
+        }
+        System.out.println(myList);
+        for (int i = myList.size() - 1; i >= 0; i--) {
+            if (myList.get(i) % 2 == 0) {
+                myList.remove(i);
             }
         }
-        System.out.println(Arrays.toString(mas));
+        System.out.println(myList);
     }
+
 }
--- task2.java
+++ task2.java
@@ -1,40 +1,20 @@
-// К калькулятору из предыдущего дз добавить логирование.
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка.
 
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.logging.*;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.*;
 
 public class task2 {
-    public static void main(String[] args) throws IOException {
-        Logger logger = Logger.getLogger(task2.class.getName());
-        FileHandler fh = new FileHandler("log.txt");
-        logger.addHandler(fh);
-        SimpleFormatter sFormat = new SimpleFormatter(); 
-        fh.setFormatter(sFormat);
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите первое число ");
-        int num1 = scan.nextInt();
-        System.out.print("Введите знак ");
-        char mark = scan.next().charAt(0);
-        System.out.print("Введите второе число ");
-        int num2 = scan.nextInt();
-        scan.close();
-        if (mark == '+') {
-            logger.log(Level.INFO, "Числа были сложены");
-            System.out.println(num1 + num2);
+    public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>(20);
+        for (int i = 0; i < 20; i++) {
+            myList.add(new Random().nextInt(0, 100));
         }
-        else if (mark == '-') {
-            logger.log(Level.INFO, "Числа были вечеты");
-            System.out.println(num1 - num2);
-        }
-        else if (mark == '*') {
-            logger.log(Level.INFO, "Числа были умножены");
-            System.out.println(num1 * num2);
-        }
-        if (mark == '/') {
-            logger.log(Level.INFO, "Числа были поделены");
-            System.out.println(num1 / num2);
-        }
+        System.out.println(myList);
+        int min = Collections.min(myList);
+        int max = Collections.max(myList);
+        double avrg = myList.stream().mapToDouble(a -> a).sum() / myList.size();
+        NumberFormat numFormat = new DecimalFormat("#.######");
+        System.out.printf("min: %d, max: %d, average: %s\n", min, max, numFormat.format(avrg));
     }
 }
-
